8a56a75b753dbd5ce2a2f2a3b8de8ca002a6b7c9
Юра <naerok@icloud.com>
2021-06-10 13:49:30 +0300

    add components

---
+++ src/components/FeedbackForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <RateUs />
+  </div>
+</template>
+
+<script>
+import RateUs from "./RateUs.vue";
+
+export default {
+  components: {
+    RateUs,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
